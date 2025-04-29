5dd41b96aebfd36aec938941cf2c4a1540ae6c94
iknowmagic <ilan@iknowmagic.net>
2025-04-29 10:08:15 -0700

    chore: update version to 1.2.0 and add plop for component generation

---
+++ plopfile.js
@@ -0,0 +1,34 @@
+export default function (plop) {
+  plop.setGenerator('component', {
+    description: 'Generate a React component folder structure',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name (PascalCase please):',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: '.templates/Component.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/index.ts',
+        templateFile: '.templates/index.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx',
+        templateFile: '.templates/Component.test.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.types.ts',
+        templateFile: '.templates/Component.types.ts.hbs',
+      },
+    ],
+  })
+}
